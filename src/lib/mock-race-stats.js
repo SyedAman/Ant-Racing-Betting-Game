dccd965f588225b9f8480247dc44ff258d1d7f7c
Syed Aman <sna274@nyu.edu>
2017-10-30 18:54:28 -0500

    create mock race stats

---
+++ src/lib/mock-race-stats.js
@@ -0,0 +1,22 @@
+const  mockRaceStats = [
+  {
+    name: 'Grand Prix II',
+    winners: [],
+    status: 'upcoming'
+  },
+  {
+    name: 'Mud Creek Drive',
+    winners: [],
+    status: 'upcoming'
+  },
+  {
+    name: 'O’r the Mountains',
+    winners: [],
+    status: 'upcoming'
+  },
+  {
+    name: 'Ork’s Grave',
+    winners: [],
+    status: 'upcoming'
+  }
+]
