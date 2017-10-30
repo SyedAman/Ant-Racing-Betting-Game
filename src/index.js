9cef3fc48bbf9b6f6c3fc8c6485f256517cecf89
Syed Aman <sna274@nyu.edu>
2017-10-30 12:05:43 -0500

    move app container into a container folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import './index.css'
-import App from './App'
+import App from './containers/App/App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
