3b0c176a13bbeffc0f661488fb996d7db8f031be
Syed Aman <sna274@nyu.edu>
2017-10-30 12:01:48 -0500

    install material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider } from 'material-ui/styles'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
-    );
+      <MuiThemeProvider>
+        <div className="App">
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
+
+// inject touch and tapping functionality needed for Material UI
+injectTapEventPlugin()
