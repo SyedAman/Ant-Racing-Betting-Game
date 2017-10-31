7a551f772f77810a82489fc4d222b6f534edc1b6
Syed Aman <sna274@nyu.edu>
2017-10-31 14:15:38 -0500

    create the TabBar component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { MuiThemeProvider } from 'material-ui/styles'
 
 import NavBar from '../../components/NavBar'
+import TabBar from '../../components/TabBar'
 import Upcoming from '../Upcoming'
 
 import './App.css';
@@ -12,6 +13,7 @@ class App extends Component {
       <MuiThemeProvider>
         <div className="App">
           <NavBar title="Match Center" />
+          <TabBar />
           <Upcoming />
         </div>
       </MuiThemeProvider>
