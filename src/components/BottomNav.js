50ad3d0a0262c3b12147570effd3d970f0349526
Syed Aman <sna274@nyu.edu>
2017-11-01 09:17:06 -0500

    add docs to bottom nav

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import BottomNavigation, {BottomNavigationButton } from 'material-ui/BottomNavigation';
 
+/**
+ * Navigation bar on the bottom of the screen that takes users to key locations.
+ * @method BottomNav
+ * @param {Object} props - Data passed in from parent component as properties.
+ */
 const BottomNav = props => (
   <BottomNavigation>
+    {/* Takes user to Upcoming page. */}
     <BottomNavigationButton
       label="Upcoming"
       icon={<i className="material-icons">event</i>}
     />
 
+    {/* Takes user to Recent page. */}
     <BottomNavigationButton
       label="Recent"
       icon={<i className="material-icons">done</i>}
