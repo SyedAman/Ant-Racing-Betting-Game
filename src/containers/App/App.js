b942f73c34adc6c538fad83a6acdf398799de495
Syed Aman <sna274@nyu.edu>
2017-11-01 09:33:39 -0500

    add a heading to the upcoming races

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Typography from 'material-ui/Typography';
 
 import NavBar from '../../components/NavBar';
 import BottomNav from '../../components/BottomNav';
@@ -10,8 +11,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* Top navigation. */}
         <NavBar title="Match Center" />
+
+        <Typography type="subheading" align="left" gutterbottom>
+          Upcoming Races
+        </Typography>
         <Upcoming />
+
+        {/* Bottom navigation. */}
         <BottomNav />
       </div>
     );
