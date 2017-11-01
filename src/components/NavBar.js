e8e8b375146fa1b738600e5de61b23c94edfa5c5
Syed Aman <sna274@nyu.edu>
2017-11-01 12:07:53 -0500

    remove unneccessary imports

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
 
 /**
  * The top navigation bar displaying important user actions and navigation.
