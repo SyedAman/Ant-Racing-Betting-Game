05b096fb35e8fc12bfe0365d2f23ac24051c69dc
Syed Aman <sna274@nyu.edu>
2017-11-01 09:39:19 -0500

    use roboto font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import 'typeface-roboto' // roboto font
 
-import './index.css'
 import App from './containers/App/App'
 import registerServiceWorker from './lib/registerServiceWorker'
+
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
